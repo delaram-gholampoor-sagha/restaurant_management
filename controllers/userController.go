90d907096cba032b7b82b3b835637a51bb8e66e2
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-09 20:53:57 +0330

    outline of the functions (userController)

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,37 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+
+}
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func LogIn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func HashPassword(password string) string {
+	return
+}
+
+func Verifypassword(userPassword string, providedPassword string) (bool, string) {
+	return
+
+}
