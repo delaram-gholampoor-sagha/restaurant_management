5be29edee3f8b8c29aef94a48b5eb020f8dc52c0
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-09 15:26:29 +0330

    added orderItemRoute

--- routes/orderItemRouter.go
+++ routes/orderItemRouter.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	controller "github.com/Delaram-Gholampoor-Sagha/restaurant_management/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func OrderItemRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/orderItems", controller.GetOrderItems())
+	incomingRoutes.GET("/orderItems/:orderItem_id", controller.GetOrderItem())
+	// if i have an order_id i want to get all the order_items for that order_id
+	incomingRoutes.GET("/orderItems-order/:order_id", controller.GetOrderItemsByOrder())
+	incomingRoutes.POST("/orderItems", controller.CreateOrderItem())
+	incomingRoutes.PATCH("/orderItems/:orderItem_id", controller.UpdateOrderItem())
+
+}
