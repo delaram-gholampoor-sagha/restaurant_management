07fb18e830fdd96cbe0f635252bcc0d3fc23f8e3
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-09 14:57:56 +0330

    added tableRoute

--- routes/tableRouter.go
+++ routes/tableRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	controller "github.com/Delaram-Gholampoor-Sagha/restaurant_management/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func TableRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/tables", controller.GetTables())
+	incomingRoutes.GET("/tables/:table_id", controller.GetTable())
+	incomingRoutes.POST("/tables", controller.CreateTable())
+	incomingRoutes.PATCH("/table/:table_id", controller.UpdateTable())
+}
