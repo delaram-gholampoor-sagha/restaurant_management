5d8a098712eb252e3b07ae4f3fb453b8fd78cdc4
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-09 21:20:35 +0330

    outline for the functions (menuController)

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,1 +1,21 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func GetMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func CreateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func UpdateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
