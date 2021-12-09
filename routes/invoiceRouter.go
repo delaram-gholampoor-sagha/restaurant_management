aa395f675af1ba45ec31ed562869dcc3dcb161f2
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-09 13:00:08 +0330

    added invoiceRoutes

--- routes/invoiceRouter.go
+++ routes/invoiceRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	controller "github.com/Delaram-Gholampoor-Sagha/restaurant_management/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func InvoiceRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/invoices", controller.GetInvoices())
+	incomingRoutes.GET("/incoices/:invoice_id", controller.GetInvoice())
+	incomingRoutes.POST("/invoices", controller.CreateInvoice())
+	incomingRoutes.PATCH("/invoices/:invoice_id", controller.UpdateInvoice())
+
+}
