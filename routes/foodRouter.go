d9dac9717825f1945abe3ba5612480f63fa2cca3
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-09 10:01:24 +0330

    added userRoute function

--- routes/foodRouter.go
+++ routes/foodRouter.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	controller "github.com/Delaram-Gholampoor-Sagha/restaurant_management/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func FoodRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/foods", controller.GetFoods())
+	incomingRoutes.GET("/foods/:food_id", controller.GetFoods())
+	// to create a food item
+	incomingRoutes.POST("/foods", controller.CreateFood())
+	// to update a food item
+	incomingRoutes.PATCH("/foods/:food_id", controller.UpdaeFood())
+}
