229d63812e978071eeb43ed900311488c2c59094
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-09 14:35:23 +0330

    added menuRoute

--- routes/menuRouter.go
+++ routes/menuRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	controller "github.com/Delaram-Gholampoor-Sagha/restaurant_management/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func MenuRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/menus", controller.GetMenus())
+	incomingRoutes.GET("/menus/:menu_id", controller.GetMenu())
+	incomingRoutes.POST("/menus", controller.CreateMenu())
+	incomingRoutes.PATCH("/menus/:menu_id", controller.UpdateMenu())
+}
