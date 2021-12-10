3961dc3a2833146c71bb4df37c49df8998d14e14
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-10 16:20:41 +0330

    worked on getMenu and getMenus

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,15 +1,50 @@
 package controller
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Delaram-Gholampoor-Sagha/restaurant_management/database"
+	"github.com/Delaram-Gholampoor-Sagha/restaurant_management/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
+
 func GetMenu() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		menuId := c.Param("menu_id")
+		var menu models.Menu
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": menuId}).Decode(&menu)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the menu item"})
+			return
+		}
+		c.JSON(http.StatusOK, menu)
+
+	}
 }
 
 func GetMenus() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing the menu items"})
+		}
+		var allMenu bson.M
+		if err = result.All(ctx, &allMenu); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenu)
+	}
 }
 
 func CreateMenu() gin.HandlerFunc {
