96d0917dff2d1980763ebf2f2ac6071a2a3d1f00
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-10 19:32:14 +0330

    worked on getFoods

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -3,7 +3,9 @@ package controller
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/Delaram-Gholampoor-Sagha/restaurant_management/database"
@@ -21,6 +23,49 @@ var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 10
+		}
+
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1
+		}
+
+		startIndex := (page - 1) * recordPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		//it matches records using a certain criteria
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+
+		// group using that criteria - for example i want all the records to be grouped by name it will takeou all the unique names and then group the records using that criteria
+		groupStage := bson.D{{"$group", bson.D{{"_id", "null"}}}, {"total_count", bson.D{{"$sum", 1}}}, {"data", bson.D{{"$push", "$$ROOT"}}}}
+		//it determines what values should go to the frontend
+		projectStage := bson.D{
+			{
+				"$project", bson.D{
+					{"_id", 0},
+					{"total_count", 1},
+					{"food_item", bson.D{{"$slice", []interface{}{"$data", startIndex, recordPerPage}}}},
+				},
+			},
+		}
+
+		result, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage,
+		})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing the food items"})
+		}
+
+		var allFoods []bson.M
+		if err := result.All(ctx, &allFoods); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allFoods[0])
 
 	}
 }
