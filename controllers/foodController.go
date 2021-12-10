1508213d8829f0dd7fad55e9f576c62d8e38959f
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-10 15:44:48 +0330

    worked on createFood and getFood

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,17 +1,92 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Delaram-Gholampoor-Sagha/restaurant_management/database"
+	"github.com/Delaram-Gholampoor-Sagha/restaurant_management/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/go-playground/validator.v8"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
+
+var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+
+	}
 }
 
 func GetFood() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Param("food_id")
+		var food models.Food
+		// so we use the food_id to find the a particular food item (the whole object of the food item) then we
+		// use the decode method to convert that into something that golang understand
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food item"})
+		}
+		c.JSON(http.StatusOK, food)
+
+	}
 }
 
 func CreateFood() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+		var food models.Food
+
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(food)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+		// food_item belongs to a menu so we have to first make sure that the menu exist .. how ? by its id !
+		// as we alreedy seen to create a food item you need to enter a menu_id
+		err := menuCollection.Find(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+		defer cancel()
+		if err != nil {
+			msg := fmt.Sprintf("menu was not found")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			err
+		}
+
+		food.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.ID = primitive.NewObjectID()
+		food.Food_id = food.ID.Hex()
+		var num = toFixed(*food.Price, 2)
+		food.Price = &num
+
+		result, insetionErr := foodCollection.InsertOne(ctx, food)
+		if insetionErr != nil {
+			msg := fmt.Sprintf("Food item was not creted")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		defer cancel()
+		c.JSON(http.StatusOK, result)
+	}
 }
 
 func UpdateFood() gin.HandlerFunc {
--- go.mod
+++ go.mod
@@ -28,5 +28,6 @@ require (
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
 	golang.org/x/text v0.3.5 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
