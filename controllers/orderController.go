c9e45ecda9f3b6845651f541fb5dec3a79ab97b9
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-11 11:22:02 +0330

    worked on createOrder

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -12,10 +12,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var orderCollection *mong.Collection = database.OpenCollection(database.Client, "order")
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client , "table")
 
 func GetOrders() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -50,7 +52,52 @@ func GetOrder() gin.HandlerFunc {
 }
 
 func CreateOrder() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx , cancel = context.WithTimeout(context.Background() , 100*time.Second)
+		var table models.Table
+		var order models.Order
+
+		if err = c.BindJSON(&order) ; err != nil {
+			c.JSON(http.StatusBadRequest , gin.H{"error" : err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(order)
+
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest , gin.H{"error" : validationErr.Error()})
+			return
+		}
+
+		// table id needs to be ther before you create the order table
+		if order.Table_id != nil {
+			err := tableCollection.FindOne(ctx , bson.M{"table_id" : order.Table_id}).Decode(&table)
+			defer cancel()
+			if err != nil {
+				msg := fmt.Sprintf("message: table was not found")
+				c.JSON(http.StatusInternalServerError , gin.H{"error" : msg})
+				return
+			}
+
+			order.Created_at , _ = time.Parse(time.RFC3339 , time.Now().Format(time.RFC3339))
+			order.Updated_at , _ = time.Parse(time.RFC3339 , time.Now().Format(time.RFC3339))
+
+			order.ID = primitive.NewObjectID()
+			order.Order_id = order.ID.Hex()
+
+
+			result , insertErr := orderCollection.InsertOne(ctx ,err)
+			if insertErr != nil {
+				msg := fmt.Sprintf("order item was not created")
+				c.JSON(http.StatusInternalServerError , gin.H{"error" : msg})
+				return
+			}
+			defer cancel()
+			c.JSON(http.StatusOk , result)
+			
+
+		}
+	}
 }
 
 func UpdateOrder() gin.HandlerFunc {
