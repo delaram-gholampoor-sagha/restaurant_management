be80f7f2f69c3d3688b46acbfe289c4c8752875f
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-12 14:16:18 +0330

    finished tableController

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,25 +1,112 @@
 package controller
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Delaram-Gholampoor-Sagha/restaurant_management/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func GetTables() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := tableCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while lisitng table items"})
+			return
+		}
+
+		var allTables []bson.M
+		if err = result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allTables)
+
+	}
 }
 
 func GetTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		tableId := c.Param("table_id")
+
+		var table models.Table
+
+		err := tableCollection.FindOne(ctx, bson.M{"table_id": tableId}).Decode(&table)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the tables"})
+			return
+		}
+		c.JSON(http.StatusOK, table)
 
 	}
 }
 
 func CreateTable() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var table models.Table
+
+		if err := c.BindJSON(&table); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		//before we send anything to the database , we validate our struct based on the model that we already created
+		validationErr := validate.Struct(table)
+
+		if validationErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": validationErr.Error()})
+			return
+		}
+		table.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		table.ID = primitive.NewObjectID()
+		table.Table_id = table.ID.Hex()
+
+		result, insertErr := tableCollection.InsertOne(ctx, table)
+		if insertErr != nil {
+			msg := fmt.Sprintf("Table item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		defer cancel()
+
+		c.JSON(http.StatusOK, result)
+	}
 }
 
 func UpdateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var table models.Table
+		tableId := c.Param("table_id")
+
+		if err := c.BindJSON(&table); err != nil {
+			log.Fatal(err)
+		}
+
+		var updatedObj primitive.D
+
+		if table.Number_of_guests 
+
+
+		if table.Table_number
+
+
+		table.Updated_at , _ = time.Parse(time.RFC3339 , time.Now().Format(time.RFC3339))
 
 	}
 }
